6f62e1ca51950ceb84872ca9db496f5090586aa7
Iqra Naz <iqranaz240@gmail.com>
2020-08-13 15:29:32 +0500

    updated

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,12 @@
+import 'package:attendance_app_1/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    // return either the Home or Authenticate widget
+    return Home();
+    
+  }
+}
